a175a9907b6b9c5898a5ce363345ee477076c6b0
guichen <717283466@qq.com>
2025-07-22 20:22:16 +0800

    包名

--- manimgl/manim_imports_ext.py
+++ manimgl/manim_imports_ext.py
@@ -1,5 +1,5 @@
 from manimlib import *
 from animations import SpinShowCreation, ShowRotatingCreate, RotatingCreate, SpinInFromNothing
 from typing import Callable, Iterable, Tuple, Union
-from mobject import ComplexSurfaceWireframe, CalabiYauSurface
-from utils import spiral_path
+from mobject import ComplexSurfaceWireframe, CalabiYauSurface, Hypercube
+from utils import spiral_path, rotation_matrix_4d
